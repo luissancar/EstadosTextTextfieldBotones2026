09356762839052b3a3a26ae52874564988ee4290
luis jose sanchez carrasco <luissancar@yahoo.es>
2025-10-01 10:31:24 +0200

    Tres

--- app/src/main/java/com/example/estadostexttextfieldbotones/Components/MyTextField.kt
+++ app/src/main/java/com/example/estadostexttextfieldbotones/Components/MyTextField.kt
@@ -6,6 +6,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -16,8 +22,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lint.kotlin.metadata.Visibility
+
+
 
 @Composable
 fun MyTextField(modifier: Modifier = Modifier) {
@@ -33,6 +45,7 @@ fun MyTextField(modifier: Modifier = Modifier) {
 fun MyTextFieldParent(modifier: Modifier = Modifier) {
     var user by rememberSaveable { mutableStateOf("") }
     var dato by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
 
     Column(modifier = modifier) {
         Row(verticalAlignment = Alignment.CenterVertically) {
@@ -42,7 +55,43 @@ fun MyTextFieldParent(modifier: Modifier = Modifier) {
             }
         }
         MyTextFieldDato(dato) { dato = it }
+        MyTextFieldAd(dato) { dato = it }
+        MyPasswordTextField(password) { password = it }
     }
+
+}
+
+@Composable
+fun MyPasswordTextField(password: String, onValueChange: (String) -> Unit) {
+    var visible by rememberSaveable { mutableStateOf(false) }
+    TextField(
+        password, onValueChange = { onValueChange(it) },
+        singleLine = true,
+        label = { Text("Contraseña") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        visualTransformation = if (visible) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        trailingIcon = {
+
+            val image = if (visible) {
+                Icons.Filled.Visibility
+            } else {
+                Icons.Filled.VisibilityOff
+            }
+
+            val description = if (visible) "Ocultar contraseña" else "Mostrar contraseña"
+
+            IconButton(onClick = { visible = !visible }) {
+                Icon(imageVector = image, contentDescription = description)
+            }
+
+        }
+
+
+    )
 
 }
 
@@ -80,7 +129,24 @@ fun MyBoxWithText() {
 }
 
 
+@Composable
+fun MyTextFieldAd(dato: String, onValueChange: (String) -> Unit) {
 
+    TextField(
+        dato, onValueChange = {
+            if (it.contains("a")) {
+                onValueChange(it.replace("a", "B"))
+            } else
+                if (it.contains("b")) {
+                    onValueChange(it.replace("b", "A"))
+                } else {
+                    onValueChange(it)
+                }
+        },
+        label = { Text("Introdice dato") }  // suele ir sin placeholder
+    )
+
+}
 
 
 
